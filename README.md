e27a7b5749332e55d163e3bb4d384b27654d92ba
Edgar Sierra <efsierraa@gmail.com>
2022-02-28 22:12:53 -0600

    updated folder filter_SK

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Cyclostationary analysis in angular domain for bearing fault identification
+[![Python 3.10](https://img.shields.io/badge/python-3.10-blue.svg)](https://www.python.org/downloads/release/python-3102/)
+[![Poetry](https://img.shields.io/badge/poetry-1.1.11-red.svg)](https://python-poetry.org/docs/master/#installation/)
 
 The following repository has the functions needed in order to reproduce the results of the work entitled "Cyclo-non-stationary analysis for bearing fault identification based on instantaneous angular speed estimation" presented in the https://survishno.sciencesconf.org/, the database is available as supplementary material of the article, "Feedback on the Surveillance 8 challenge: Vibration-based diagnosis of a Safran aircraft engine", founded in https://doi.org/10.1016/j.ymssp.2017.01.037. If you find this material useful, please give a citation to the papers mentioned above.
 
@@ -7,10 +9,26 @@ Functions to REB failure identification based on Spectral kurtosis:
 
   SK_W # Spectral kurtosis from a Matlab repository made by ANTONI Jerome
   
-  PSD_envW # Proposed algorithm SK based implementation.
+  PSD_envW # Proposed algorithm sliding SK based implementation.
   
   bearingfault # Bearing fault numerical signal for any questions please contact http://lva.insa-lyon.fr/en/
-  
+## Example
+``` python
+mfreq = 0.4*fr # fr rotational speed
+Nw1      = 2**np.ceil(np.log2(1/mfreq*fs))*2**3 # greater than Nw2 window for envelope spectrum
+Nw1      = int(Nw1)
+Nw2      = 2**8 # window for computation of SK
+Nw2      = int(Nw2)
+Nfft     = 2*Nw1
+Nfft2    = 2*Nw2
+Noverlap = round(3/4*Nw1)
+Window   = np.kaiser(Nw1,beta=0) # beta 0 rectangular,5	Similar to a Hamming
+# 6	Similar to a Hanning, 8.6	Similar to a Blackman
+
+#%
+filterr  = 1 # filtering with SK
+psd,f,K,SK_w  = PSD_envW(x,Nfft,Noverlap,Window,Nw2,Nfft2,filterr)
+```
   # Angular resampling functions
   These functions are translated/adapted from the book Noise and Vibration Analysis Signal Analysis and Experimental Procedures by Anders Brandt
   
@@ -28,3 +46,11 @@ Functions to REB failure identification based on Spectral kurtosis:
   printxtips
   
   printxtipsusr
+
+  
+  # References
+  ```
+  Sierra-Alonso, E. F., Caicedo-Acosta, J., Orozco Gutiérrez, Á. Á., Quintero, H. F., & Castellanos-Dominguez, G. (2021). Short-time/-angle spectral analysis for vibration monitoring of bearing failures under variable speed. Applied Sciences, 11(8), 3369.
+```
+## Revised version of the code
+  2022-02-28, revised functions to enhance impulsive components using Spectral Kurtosis in the folder filter_SK
---
+++ filter_SK/SK.py
@@ -0,0 +1,122 @@
+import numpy as np # math pack!
+import scipy.signal as sg # DSP
+from numpy import linalg as LA # Linear algebra
+import sys # sio package
+
+# %% Spectral Kurtosis
+def SK_W(x,Nfft,Noverlap,Window):
+# [SK,M4,M2,f] = SK_W(x,Nfft,Noverlap,Window) 
+# Welch's estimate of the spectral kurtosis       
+#       SK(f) = M4(f)/M2(f)^2 - 2 
+# where M4(f) = E{|X(f)|^4} and M2(f) = E{|X(f)|^2} are the fourth and
+# second order moment spectra of signal x, respectively.
+# Signal x is divided into overlapping blocks (Noverlap taps), each of which is
+# detrended, windowed and zero-padded to length Nfft. Input arguments nfft, Noverlap, and Window
+# are as in function 'PSD' or 'PWELCH' of Matlab. Denoting by Nwind the window length, it is recommended to use 
+# nfft = 2*NWind and Noverlap = 3/4*Nwind with a hanning window.
+# (note that, in the definition of the spectral kurtosis, 2 is subtracted instead of 3 because Fourier coefficients
+# are complex circular)
+#
+# --------------------------
+# References: 
+# J. Antoni, The spectral kurtosis: a useful tool for characterising nonstationary signals, Mechanical Systems and Signal Processing, Volume 20, Issue 2, 2006, pp.282-307.
+# J. Antoni, R. B. Randall, The spectral kurtosis: application to the vibratory surveillance and diagnostics of rotating machines, Mechanical Systems and Signal Processing, Volume 20, Issue 2, 2006, pp.308-331.
+# --------------------------
+# Author: J. Antoni
+# Last Revision: 12-2014
+# --------------------------
+    if type(Window) == int:
+        Window = np.hanning(Window)
+        # https://docs.scipy.org/doc/numpy/reference/generated/numpy.hanning.html
+    Window = Window.reshape(-1,1)[:,0]/LA.norm(Window)	# window normalization
+    n = len(x)  							        # number of data points
+    nwind = len(Window)     				        # length of window
+    
+    if nwind <= Noverlap:
+        print('Window length must be > Noverlap')
+        input()
+        sys.exit(1)
+    if Nfft < nwind:
+        print('Window length must be <= Nfft')
+        input()        
+        sys.exit(1)
+    
+    x = x.reshape(-1,1)[:,0]	
+    k = np.fix((n-Noverlap)/(nwind-Noverlap)) # number of windows
+    
+    
+    # Moment-based spectra
+    index = np.r_[:nwind]
+    f = np.r_[:Nfft]/Nfft;
+    M4 = 0;
+    M2 = 0;
+    
+    for i in np.r_[:k]:
+        xw = Window*x[index]
+        Xw = np.fft.fft(xw,n=Nfft)/Nfft        
+        M4 = np.abs(Xw)**4 + M4
+        M2 = np.abs(Xw)**2 + M2
+        index = index + (nwind - Noverlap)
+    
+    # normalize
+    M4 = M4/k;   
+    M2 = M2/k; 
+    
+    # spectral kurtosis 
+    SK = M4/M2**2 - 2
+    
+    # reduce bias near f = 0 mod(1/2)
+    W = np.abs(np.fft.fft(Window**2,Nfft))**2
+    Wb = np.zeros([Nfft,1])[:,0]
+    for i in np.r_[:Nfft]:
+       Wb[i] = W[np.mod(2*i,Nfft)]/W[0]
+       
+    SK = SK - Wb
+    
+    return SK,M4,M2,f
+
+# % PSD filtered with sliding Spectral Kurtosis
+def PSD_envW(x,nfft,Noverlap,Window,Nwind2,nfft2,filterr):
+# variables x,nfft,Noverlap,Window are the same as in SK_W
+# Nwind2 is the length of the sliding window it must be larger than Window
+# nfft2 is the resolution of the filtered PSD
+# filterr is the filter is a boolean variable if true the PSD is filtered with SK_W in a sliding basis
+# Author: Edgar F. Sierra-Alonso
+# Last Revision: 02-2022
+# --------------------------
+    Window = Window.reshape(-1,1)[:,0]
+    n = len(x)		# Number of data points
+    nwind = len(Window) # length of window
+    x = x.reshape(-1,1)[:,0]
+    K = int(np.fix((n-Noverlap)/(nwind-Noverlap))) # Number of windows
+    Noverlap2 = int(np.round(3/4*Nwind2))
+    Window2 = np.hanning(Nwind2)
+    
+    # compute 
+    index = np.r_[:nwind]
+    #t = np.r_[:n]
+    psd = np.zeros(nfft)
+    SK_w = np.zeros([nfft2,K])
+    for i in np.r_[:K]:
+        xw = Window*x[index]
+    	# filtering
+        if filterr == 1:
+            SK,_,_,_ = SK_W(xw,nfft2,Noverlap2,Window2)
+            SK_w[:,i] = SK
+            b = np.fft.fftshift(np.real(np.fft.ifft(SK)))
+            xw = sg.fftconvolve(xw,b,mode='same') #xw = fftfilt(b,xw);
+        xw = np.abs(sg.hilbert(xw)) # envelope
+        xw = xw**2
+        xw = xw - np.mean(xw)
+        xw = sg.hilbert(xw)
+        Xw = np.fft.fft(xw,nfft)/nfft
+        psd = np.abs(Xw)**2 + psd
+        index = index + (nwind - Noverlap)
+    # normalize
+    KMU = K*LA.norm(Window)**2;	#Normalizing scale factor ==> asymptotically unbiased
+    psd = psd/KMU
+    
+    freq = np.fft.fftfreq(psd.shape[-1])
+    psd = psd[:int(len(psd)/2)]
+    freq = freq[:int(len(freq)/2)]
+    return psd,freq,K,SK_w
